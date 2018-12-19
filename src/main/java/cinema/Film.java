26ff9f52cdd953a30b2a65a1789364f0d79ceb8b
azakenaton <luccho7.lh@gmail.com>
2018-12-19 16:53:07 +0100

    changement nom package

--- src/main/java/cinema/Film.java
+++ src/main/java/cinema/Film.java
@@ -1,4 +1,4 @@
-package model;
+package cinema;
 
 public class Film {
 
@@ -38,6 +38,6 @@ public class Film {
     public void setTitre(String titre) {
         this.titre = titre;
     }
-    
+
 
 }

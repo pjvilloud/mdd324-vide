8e24212c381e650efe8e64c2627d1fab0808f22e
azakenaton <luccho7.lh@gmail.com>
2018-12-19 14:59:10 +0100

    Ajout package et de l'objet Film

---
+++ src/main/java/model/Film.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Film {
+
+    private String categorie;
+    private String decription;
+    private String durée;
+    private String titre;
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+    public String getDurée() {
+        return durée;
+    }
+
+    public void setDurée(String durée) {
+        this.durée = durée;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+    
+
+}

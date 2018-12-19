9538772262a9516b83de518906ad18209022a2de
Jordan <jordanbouget@gmail.com>
2018-12-19 16:01:17 +0100

    test App genson error

--- src/main/java/helloworld/App.java
+++ src/main/java/helloworld/App.java
@@ -1,19 +1,21 @@
 package helloworld;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import module.ListeGlobale;
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-
 /**
  * Handler for requests to Lambda function.
  */
+
 public class App implements RequestHandler<Object, Object> {
 
     public Object handleRequest(final Object input, final Context context) {
@@ -21,8 +23,9 @@ public class App implements RequestHandler<Object, Object> {
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
         try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+            final String pageContents = this.getPageContents("https://raw.githubusercontent.com/theofidry/ephemeris/master/src/ephemeris.json");
+            ListeGlobale listeGlobale = genson.deserialize(pageContents, ListeGlobale.class);
+            //String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
             return new GatewayResponse(output, headers, 200);
         } catch (IOException e) {
             return new GatewayResponse("{}", headers, 500);

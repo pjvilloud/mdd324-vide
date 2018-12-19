d84abe438eaa23743a2c68e3b3c43299e3d7b6f8
oussamaKaouachi <oussama.kaouachi@sully-group.fr>
2018-12-19 14:37:54 +0100

    class Ephemeride ajouté !

---
+++ src/main/java/module/Ephemeride.java
@@ -0,0 +1,49 @@
+package module;
+
+public class Ephemeride {
+    private String dateJour;
+    private String feteJour;
+    private Integer jourAnnee;
+    private Integer joursRestants;
+    private Integer numSemaines;
+
+    public String getDateJour() {
+        return dateJour;
+    }
+
+    public void setDateJour(String dateJour) {
+        this.dateJour = dateJour;
+    }
+
+    public String getFeteJour() {
+        return feteJour;
+    }
+
+    public void setFeteJour(String feteJour) {
+        this.feteJour = feteJour;
+    }
+
+    public Integer getJourAnnee() {
+        return jourAnnee;
+    }
+
+    public void setJourAnnee(Integer jourAnnee) {
+        this.jourAnnee = jourAnnee;
+    }
+
+    public Integer getJoursRestants() {
+        return joursRestants;
+    }
+
+    public void setJoursRestants(Integer joursRestants) {
+        this.joursRestants = joursRestants;
+    }
+
+    public Integer getNumSemaines() {
+        return numSemaines;
+    }
+
+    public void setNumSemaines(Integer numSemaines) {
+        this.numSemaines = numSemaines;
+    }
+}

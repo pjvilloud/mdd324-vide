e1876bf1c975ce504a742706e1b52857f9f61b71
azakenaton <luccho7.lh@gmail.com>
2018-12-20 16:36:17 +0100

    test parenthèse

--- src/main/java/cinema/App.java
+++ src/main/java/cinema/App.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * Handler for requests to Lambda function.
@@ -65,8 +66,9 @@ public class App implements RequestHandler<Object, Object> {
             String tempDescription = i.getDescription();
             String[] temp = tempDescription.split(" - ");
             description = temp[1] + temp[2];
-            temp = temp[0].split(" ");
-            categorie = temp[0].split(";")[2];
+            temp = temp[0].split(Pattern.quote("("));
+            String[] tempCategorie = temp[0].split(";");
+            categorie = tempCategorie[2];
             duree = temp[1].substring(1, 9);
             films.add(new Film(i.getTitle(), description, categorie, duree));
         }

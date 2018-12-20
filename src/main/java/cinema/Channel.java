c0de203123a60c3a360b8f08186bd17a302675b9
azakenaton <luccho7.lh@gmail.com>
2018-12-20 12:09:01 +0100

    Modification

--- src/main/java/cinema/Channel.java
+++ src/main/java/cinema/Channel.java
@@ -7,12 +7,14 @@ import java.util.List;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "channel")
 @XmlAccessorType(XmlAccessType.PROPERTY)
 public class Channel implements Serializable{
 
+    @XmlElement(name = "item")
     private List<Item> items;
 
     public List<Item> getItems() {

baee11fb592163439dc097c92f64628c7c165aa4
Jordan <jordanbouget@gmail.com>
2018-12-20 10:17:05 +0100

    correct genson error

--- src/main/java/module/ListeGlobale.java
+++ src/main/java/module/ListeGlobale.java
@@ -1,115 +1,115 @@
 package module;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class ListeGlobale {
-    private ArrayList<Liste> january;
-    private ArrayList<Liste> february;
-    private ArrayList<Liste> march;
-    private ArrayList<Liste> april;
-    private ArrayList<Liste> may;
-    private ArrayList<Liste> june;
-    private ArrayList<Liste> july;
-    private ArrayList<Liste> august;
-    private ArrayList<Liste> september;
-    private ArrayList<Liste> october;
-    private ArrayList<Liste> november;
-    private ArrayList<Liste> december;
+    private List<List<String>> january;
+    private List<List<String>> february;
+    private List<List<String>> march;
+    private List<List<String>> april;
+    private List<List<String>> may;
+    private List<List<String>> june;
+    private List<List<String>> july;
+    private List<List<String>> august;
+    private List<List<String>> september;
+    private List<List<String>> october;
+    private List<List<String>> november;
+    private List<List<String>> december;
 
 
-    public ArrayList<Liste> getJanuary() {
+    public List<List<String>> getJanuary() {
         return january;
     }
 
-    public void setJanuary(ArrayList<Liste> january) {
+    public void setJanuary(List<List<String>> january) {
         this.january = january;
     }
 
-    public ArrayList<Liste> getFebruary() {
+    public List<List<String>> getFebruary() {
         return february;
     }
 
-    public void setFebruary(ArrayList<Liste> february) {
+    public void setFebruary(List<List<String>> february) {
         this.february = february;
     }
 
-    public ArrayList<Liste> getMarch() {
+    public List<List<String>> getMarch() {
         return march;
     }
 
-    public void setMarch(ArrayList<Liste> march) {
+    public void setMarch(List<List<String>> march) {
         this.march = march;
     }
 
-    public ArrayList<Liste> getApril() {
+    public List<List<String>> getApril() {
         return april;
     }
 
-    public void setApril(ArrayList<Liste> april) {
+    public void setApril(List<List<String>> april) {
         this.april = april;
     }
 
-    public ArrayList<Liste> getMay() {
+    public List<List<String>> getMay() {
         return may;
     }
 
-    public void setMay(ArrayList<Liste> may) {
+    public void setMay(List<List<String>> may) {
         this.may = may;
     }
 
-    public ArrayList<Liste> getJune() {
+    public List<List<String>> getJune() {
         return june;
     }
 
-    public void setJune(ArrayList<Liste> june) {
+    public void setJune(List<List<String>> june) {
         this.june = june;
     }
 
-    public ArrayList<Liste> getJuly() {
+    public List<List<String>> getJuly() {
         return july;
     }
 
-    public void setJuly(ArrayList<Liste> july) {
+    public void setJuly(List<List<String>> july) {
         this.july = july;
     }
 
-    public ArrayList<Liste> getAugust() {
+    public List<List<String>> getAugust() {
         return august;
     }
 
-    public void setAugust(ArrayList<Liste> august) {
+    public void setAugust(List<List<String>> august) {
         this.august = august;
     }
 
-    public ArrayList<Liste> getSeptember() {
+    public List<List<String>> getSeptember() {
         return september;
     }
 
-    public void setSeptember(ArrayList<Liste> september) {
+    public void setSeptember(List<List<String>> september) {
         this.september = september;
     }
 
-    public ArrayList<Liste> getOctober() {
+    public List<List<String>> getOctober() {
         return october;
     }
 
-    public void setOctober(ArrayList<Liste> october) {
+    public void setOctober(List<List<String>> october) {
         this.october = october;
     }
 
-    public ArrayList<Liste> getNovember() {
+    public List<List<String>> getNovember() {
         return november;
     }
 
-    public void setNovember(ArrayList<Liste> november) {
+    public void setNovember(List<List<String>> november) {
         this.november = november;
     }
 
-    public ArrayList<Liste> getDecember() {
+    public List<List<String>> getDecember() {
         return december;
     }
 
-    public void setDecember(ArrayList<Liste> december) {
+    public void setDecember(List<List<String>> december) {
         this.december = december;
     }
 }

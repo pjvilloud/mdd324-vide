b39d72f21baafcbe13f8dc0a3093fb54ae500dc9
azakenaton <luccho7.lh@gmail.com>
2018-12-20 13:37:33 +0100

    Ajout getter et setter response

--- src/main/java/cinema/Response.java
+++ src/main/java/cinema/Response.java
@@ -4,9 +4,19 @@ import java.util.List;
 
 public class Response {
 
+
     private List<Film> films;
 
     public Response(List<Film> films) {
+        this.films = films;
+    }
+
+
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
         this.films = films;
     }
 }
